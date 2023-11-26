2df152e4f7422b1ecb504d9e62401b6a420ad0e5
chahirchalouati <chehhhir@gmail.com>
2023-11-26 11:30:27 +0100

    Feat: exclude spring security from gateway

--- gateway/src/main/java/com/crcl/gateway/GatewayApplication.java
+++ gateway/src/main/java/com/crcl/gateway/GatewayApplication.java
@@ -3,10 +3,12 @@ package com.crcl.gateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.cloud.gateway.filter.GatewayMetricsFilter;
+import org.springframework.cloud.gateway.handler.predicate.PathRoutePredicateFactory;
 
 @ConfigurationPropertiesScan("com.crcl.gateway.configurations")
 @SpringBootApplication
-public class GatewayApplication {
+public class GatewayApplication extends PathRoutePredicateFactory {
     public static void main(String[] args) {
         SpringApplication.run(GatewayApplication.class, args);
     }
