4d917e031ca2dd7392a8b0b01cc26ba53c4db196
chahirchalouati <chehhhir@gmail.com>
2023-12-03 11:30:51 +0100

    Feat: refactor code

--- iam/src/main/java/com/crcl/iam/configurations/security/DefaultSecurityConfig.java
+++ iam/src/main/java/com/crcl/iam/configurations/security/DefaultSecurityConfig.java
@@ -8,15 +8,12 @@ import org.jetbrains.annotations.NotNull;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
-import org.springframework.http.converter.FormHttpMessageConverter;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.annotation.web.configurers.AuthorizeHttpRequestsConfigurer;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.web.client.RestTemplate;
 
 @EnableWebFluxSecurity
 @Configuration(proxyBeanMethods = false)
@@ -25,35 +22,30 @@ public class DefaultSecurityConfig {
     private final CorsCustomizer corsCustomizer;
     private final SecurityProperties securityProperties;
 
-    @NotNull
-    private static Customizer<AuthorizeHttpRequestsConfigurer<HttpSecurity>.AuthorizationManagerRequestMatcherRegistry> getHttpRequestsCustomizer() {
-        return registry -> registry.requestMatchers(EndpointsUtils.Permitted.SWAGGER_END_POINTS).permitAll()
-                .requestMatchers(EndpointsUtils.Permitted.ACTUATOR_END_POINTS).permitAll()
-                .requestMatchers(HttpMethod.POST, "/users/**").permitAll()
-                .requestMatchers("/authentication/login/**").permitAll()
-                .requestMatchers("/authentication/register/**").permitAll()
-                .requestMatchers("/authentication/roles/**", "/authentication/permissions/**").hasAnyRole("ADMIN")
-                .anyRequest().authenticated();
-    }
-
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         this.corsCustomizer.corsCustomizer(http);
-        http.authorizeHttpRequests(
-                        registry -> registry.requestMatchers(EndpointsUtils.Permitted.SWAGGER_END_POINTS).permitAll()
-                                .requestMatchers(EndpointsUtils.Permitted.ACTUATOR_END_POINTS).permitAll()
-                                .requestMatchers(HttpMethod.POST, "/users/**").permitAll()
-                                .requestMatchers("/authentication/login/**").permitAll()
-                                .requestMatchers("/authentication/register/**").permitAll()
-                                .requestMatchers("/authentication/roles/**", "/authentication/permissions/**").hasAnyRole("ADMIN")
-                                .anyRequest().authenticated()
-                ).csrf(AbstractHttpConfigurer::disable)
+        http.authorizeHttpRequests(getHttpRequestsCustomizer())
+                .csrf(AbstractHttpConfigurer::disable)
                 .formLogin(loginConfigurer -> loginConfigurer
                         .loginPage(securityProperties.getLoginPage())
                         .failureForwardUrl(securityProperties.getFailureForwardUrl()))
                 .oauth2ResourceServer(configurer -> configurer.jwt(Customizer.withDefaults()));
 
         return http.build();
+    }
+
+    @NotNull
+    private static Customizer<AuthorizeHttpRequestsConfigurer<HttpSecurity>.AuthorizationManagerRequestMatcherRegistry> getHttpRequestsCustomizer() {
+        return registry -> registry
+                .requestMatchers(EndpointsUtils.Permitted.SWAGGER_END_POINTS).permitAll()
+                .requestMatchers(EndpointsUtils.Permitted.ACTUATOR_END_POINTS).permitAll()
+                .requestMatchers(HttpMethod.POST, "/users/**").permitAll()
+                .requestMatchers("/authentication/login/**").permitAll()
+                .requestMatchers("/authentication/register/**").permitAll()
+                .requestMatchers("/authentication/roles/**", "/authentication/permissions/**").hasAnyRole("ADMIN")
+                .anyRequest()
+                .authenticated();
     }
 }
 
