0f7a9763b1e52c8a3d3a80312de0e190faea3ec9
Chahir Chalouati <chehhhir@gmail.com>
2024-01-01 19:46:28 +0100

    Feat: Add pagination of user entity

--- iam/src/main/java/com/crcl/iam/repositories/CustomUserRepository.java
+++ iam/src/main/java/com/crcl/iam/repositories/CustomUserRepository.java
@@ -1,4 +1,10 @@
 package com.crcl.iam.repositories;
 
+import com.crcl.iam.domain.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import reactor.core.publisher.Mono;
+
 public interface CustomUserRepository {
+    Mono<Page<User>> findAll(Pageable pageable);
 }
--- iam/src/main/java/com/crcl/iam/repositories/impl/CustomUserRepositoryImpl.java
+++ iam/src/main/java/com/crcl/iam/repositories/impl/CustomUserRepositoryImpl.java
@@ -1,8 +1,15 @@
 package com.crcl.iam.repositories.impl;
 
+import com.crcl.iam.domain.User;
 import com.crcl.iam.repositories.CustomUserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.mongodb.core.ReactiveMongoTemplate;
+import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.stereotype.Repository;
+import reactor.core.publisher.Mono;
 
 /**
  * Implementation of the {@link CustomUserRepository} interface. It provides methods for querying and manipulating user data in the database.
@@ -11,4 +18,15 @@ import org.springframework.stereotype.Repository;
 @RequiredArgsConstructor
 public class CustomUserRepositoryImpl implements CustomUserRepository {
 
+    private final ReactiveMongoTemplate reactiveMongoTemplate;
+
+    @Override
+    public Mono<Page<User>> findAll(final Pageable pageable) {
+        final Query query = new Query().with(pageable);
+        return reactiveMongoTemplate.find(query, User.class)
+                .collectList()
+                .flatMap(users -> reactiveMongoTemplate.count(new Query(), User.class)
+                        .map(total -> new PageImpl<>(users, pageable, total))
+                );
+    }
 }
